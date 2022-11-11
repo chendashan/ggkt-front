0c3bac6810655d623f238ebdfa72f28bce48825d
陈大山 <chendashan@tiange.com>
2022-11-11 11:15:37 +0800

    上传头像

--- vue-admin-template/src/views/vod/teacher/form.vue
+++ vue-admin-template/src/views/vod/teacher/form.vue
@@ -31,6 +31,16 @@
 
         <!-- 讲师头像 -->
         <el-form-item label="讲师头像">
+            <el-upload
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload"
+                :on-error="handleAvatarError"
+                :action="BASE_API+'/admin/vod/file/upload'"
+                class="avatar-uploader">
+                <img v-if="teacher.avatar" :src="teacher.avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"/>
+            </el-upload>
         </el-form-item>
 
         <el-form-item>
@@ -48,7 +58,8 @@ export default {
             teacher:{
                 sort: 0,
                 level: 1
-            }
+            },
+            BASE_API: 'http://localhost:8301'
         }
     },
     created() {
@@ -96,6 +107,39 @@ export default {
                 //跳转列表页面
                 this.$router.push({path:'/vod/teacher/list'})
               })
+        },
+
+        // 上传成功回调
+        handleAvatarSuccess(res, file) {
+            // console.log(res)
+            if (res.code==20000) {
+                // console.log(res)
+                this.teacher.avatar = res.data
+                // 强制重新渲染
+                this.$forceUpdate()
+            } else {
+                this.$message.error('上传失败 （非0）')
+            }
+        },
+
+        // 错误处理
+        handleAvatarError() {
+            console.log('error')
+            this.$message.error('上传失败（http失败）')
+        },
+
+        // 上传校验
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg' || file.type === 'image/png'
+            const isLt2M = file.size / 1024 / 1024 < 2
+
+            if (!isJPG) {
+                this.$message.error('上传头像图片只能是 JPG 或 PNG 格式!')
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!')
+            }
+            return isJPG && isLt2M
         }
     }
 }
